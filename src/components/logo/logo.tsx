d37b49eb5a526f6e289102d6d1a1f95bb6acc73b
MaxHiit <pro.maxcode@gmail.com>
2023-07-14 00:59:41 +0200

    feat(component | logo): logo component | css of logo

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,12 @@
+import '@fontsource/cinzel-decorative';
+import './logo.scss';
+
+const Logo = () => {
+	return (
+		<div className='logo-wrapper'>
+			<h2 className='logo'>Game of Throne</h2>
+		</div>
+	);
+};
+
+export default Logo;
